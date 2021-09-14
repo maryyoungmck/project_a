522ddd62fd255ec52cfc44637ecd9a131da49197
maryyoungmck <marybyoung@gmail.com>
2021-09-14 22:53:36 +0100

    Version 02

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -14,11 +13,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-sleep = 'z'
-print(sleep*5)
-list = [1,2,3,5]
-print(list)
-newlist = list *2
-print(newlist)
-for i in list:
-    print (i *2)
+import pandas as pd
+
+
+
